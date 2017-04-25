1c60ceab82552802f0dd16d154125e066321565c
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2017-04-25 01:40:36 -0300

    Started model implementation

---
+++ AdditionalActivities/Model/Activity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalActivities.Model
+{
+    class Activity:IDatabaseObject
+    {
+        public IDatabaseObject Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        IDatabaseObject IDatabaseObject.Fetch()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
