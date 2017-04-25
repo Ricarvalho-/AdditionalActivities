02ade191b5153e5bbd71c3d31d6e86e3ec74c92b
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2017-04-25 01:39:40 -0300

    Modified project structure (MVC)

--- AdditionalActivities/View/StartForm.cs
+++ AdditionalActivities/View/StartForm.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace AdditionalActivities
+namespace AdditionalActivities.View
 {
-    public partial class Form1 : Form
+    public partial class StartForm : Form
     {
-        public Form1()
+        public StartForm()
         {
             InitializeComponent();
         }
