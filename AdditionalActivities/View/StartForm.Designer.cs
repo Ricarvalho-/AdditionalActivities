c43ff1f74e3267f5e027d99f41c62fda0379875a
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2017-05-06 04:09:33 -0300

    Started view implementation

--- AdditionalActivities/View/StartForm.Designer.cs
+++ AdditionalActivities/View/StartForm.Designer.cs
@@ -28,11 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartForm));
+            this.mainSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.subSplitContainer = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).BeginInit();
+            this.mainSplitContainer.Panel2.SuspendLayout();
+            this.mainSplitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.subSplitContainer)).BeginInit();
+            this.subSplitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainSplitContainer
+            // 
+            this.mainSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainSplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.mainSplitContainer.Name = "mainSplitContainer";
+            // 
+            // mainSplitContainer.Panel2
+            // 
+            this.mainSplitContainer.Panel2.Controls.Add(this.subSplitContainer);
+            this.mainSplitContainer.Size = new System.Drawing.Size(284, 261);
+            this.mainSplitContainer.SplitterDistance = 94;
+            this.mainSplitContainer.TabIndex = 0;
+            // 
+            // subSplitContainer
+            // 
+            this.subSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.subSplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.subSplitContainer.Name = "subSplitContainer";
+            this.subSplitContainer.Size = new System.Drawing.Size(186, 261);
+            this.subSplitContainer.SplitterDistance = 62;
+            this.subSplitContainer.TabIndex = 0;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Start";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.mainSplitContainer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "StartForm";
+            this.Text = "Additional Activities";
+            this.mainSplitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).EndInit();
+            this.mainSplitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.subSplitContainer)).EndInit();
+            this.subSplitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer mainSplitContainer;
+        private System.Windows.Forms.SplitContainer subSplitContainer;
     }
 }
