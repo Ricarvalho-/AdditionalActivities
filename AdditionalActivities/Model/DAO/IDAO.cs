35ab3d673c371d11a56e209651aace7320313fdf
Kauê Rodrigues <kauefr@gmail.com>
2017-05-27 23:50:15 -0300

    dao interface and database manager

---
+++ AdditionalActivities/Model/DAO/IDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalActivities.Model.DAO
+{
+    interface IDAO<T>
+    {
+        void Save(T obj);
+        void Delete(T obj);
+        T Read(int id);
+        List<T> GetAll();
+        List<T> GetByParentID(int id);
+    }
+}
---
+++ AdditionalActivities/Model/DatabaseManager.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalActivities.Model
+{
+    class DatabaseManager
+    {
+        private static DatabaseManager instance;
+
+        private static MySqlConnection connection;
+        private static string connectionString = "Server=localhost; Database=activities; Uid=ifsp; Pwd=ifsp";
+
+        private DatabaseManager()
+        {
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public static DatabaseManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DatabaseManager();
+                }
+                return instance;
+            }
+        }
+
+        public MySqlConnection Connection
+        {
+            get
+            {
+                return connection;
+            }
+        }
+    }
+}
