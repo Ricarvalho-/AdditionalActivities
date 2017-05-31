99def1ddde5e7f87f391e424b7b9bab3de90c163
Kauê Rodrigues <kauefr@gmail.com>
2017-05-31 12:38:50 -0300

    Evaluator DO, DAOException, started EvaluatorDAO

---
+++ AdditionalActivities/Model/DAO/EvaluatorDAO.cs
@@ -0,0 +1,56 @@
+﻿using AdditionalActivities.Model.Domain;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalActivities.Model.DAO
+{
+    class EvaluatorDAO : IDAO<Evaluator>
+    {
+        public void Delete(Evaluator obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Evaluator> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Evaluator> GetByParentID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Evaluator Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Evaluator obj)
+        {
+            MySqlConnection connection = DatabaseManager.Instance.Connection;
+            string cmdText = obj.Id.HasValue ?
+                "update evaluator set name = @name where id = @id;" :
+                "insert into evaluator (name) values (@name);";
+            //params
+            MySqlCommand command = new MySqlCommand(cmdText, connection);
+
+            try
+            {
+                //open, execute, update id (if needed)
+            }
+            catch (MySqlException e)
+            {
+                throw new DAOException(e.Message);
+            }
+            finally
+            {
+                //close
+            }
+        }
+    }
+}
---
+++ AdditionalActivities/Model/Domain/Evaluator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalActivities.Model.Domain
+{
+    class Evaluator
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+    }
+}
