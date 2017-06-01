b0854c02713d44b767817fcb5841cc34e5335822
Kauê Rodrigues <kauefr@gmail.com>
2017-05-31 22:59:00 -0300

    database creation script - course/rule/activity tables

--- activities.sql
+++ activities.sql
@@ -1,6 +1,36 @@
 #Database creation script
 
+drop table activity;
+drop table rule;
+drop table course;
 drop table evaluator;
+
+create table course (
+	id int not null auto_increment,
+	name varchar(255),
+	minDuration int,
+	maxDuration int,
+	primary key(id)
+);
+
+create table rule (
+	id int not null auto_increment,
+	reqHours int,
+	courseID int,
+	primary key(id)
+	foreign key(courseID) references course(id)
+);
+
+create table activity (
+	id int not null auto_increment,
+	description varchar(255),
+	minHours int,
+	maxHours int,
+	hourStep int,
+	ruleID int,
+	primary key(id),
+	foreign key(ruleID) references rule(id)
+);
 
 create table evaluator (
 	id int not null auto_increment,
